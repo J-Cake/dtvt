e0514dc0faff068e28a1cbf51f797f77b6c0dc57
jcake <jakieschneider13@gmail.com>
2022-04-21 19:49:01 +1000

    Using a more synchronous build approach. This slows the build down dramatically (usually it completes in a small fraction of a second, now it takes several seconds), but we avoid memory issues.

--- build.mjs
+++ build.mjs
@@ -2,19 +2,21 @@ import fs from 'node:fs';
 import rl from 'node:readline';
 import cp from 'node:child_process';
 
-for await (const i of rl.createInterface(fs.createReadStream(process.argv[2]))) {
-    const [dest, src, ...args] = i.split(',');
+for await (const i of rl.createInterface(fs.createReadStream(process.argv[2])))
+    await new Promise(function (resolve, reject) {
+        const [dest, src, ...args] = i.split(',');
 
-    console.error(`${src.trim()} => ${dest.trim()}`);
+        console.error(`${src.trim()} => ${dest.trim()}`);
 
-    const proc = cp.spawn('node', [process.env.TEMPLATE, ...args.map(i => i.split(':').map(i => decodeURIComponent(i.trim()))).flat(1)], {
-        cwd: process.cwd(),
-        env: process.env
-    });
+        const proc = cp.spawn('node', [process.env.TEMPLATE, ...args.map(i => i.split(':').map(i => decodeURIComponent(i.trim()))).flat(1)], {
+            cwd: process.cwd(),
+            env: process.env
+        });
 
-    proc.on('exit', code => code != 0 ?? new Promise(() => process.exit(code)));
+        proc.on('exit', code => code === 0 ? resolve(code) : reject(code));
 
-    proc.stdout.pipe(fs.createWriteStream(dest.trim()));
-    proc.stderr.pipe(process.stderr);
-    fs.createReadStream(src.trim()).pipe(proc.stdin);
-}
+        proc.stdout.pipe(fs.createWriteStream(dest.trim()));
+        proc.stderr.pipe(process.stderr);
+        fs.createReadStream(src.trim()).pipe(proc.stdin);
+
+    }).catch(code => process.exit(code));
