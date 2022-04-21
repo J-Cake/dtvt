5411a13f6e9657ac82637c683523a352db514fcf
jcake <jakieschneider13@gmail.com>
2022-04-21 23:45:02 +1000

    Added the IT Jobs section. Broke everything in the process - It was a mission

--- assets/src/grid-mgr.js
+++ assets/src/grid-mgr.js
@@ -1,4 +1,5 @@
-const resize = function () {
+const resize = window.resize = function () {
+    // const buffer = [document.querySelector('#buffer'), ...document.querySelectorAll('.tab .info')];
     const sh = [document.querySelector('#sh'), ...document.querySelectorAll('.tab .sh')];
     const pb = [document.querySelector('#pb'), ...document.querySelectorAll('.tab .pb')];
     const js = [document.querySelector('#js'), ...document.querySelectorAll('.tab .js')];
@@ -7,8 +8,11 @@ const resize = function () {
     const td = [document.querySelector('#td'), ...document.querySelectorAll('.tab .td')];
     const extra = [document.querySelector('#extra'), ...document.querySelectorAll('.tab .extra')];
 
-    for (const i of [/*info, */sh, pb, js, cp, njl, td]) {
-        const maxHeight = i.reduce((a, i) => Math.max(a, i.clientHeight), 0);
+    for (const i of [sh, pb, js, cp, njl, td, extra]) {
+        for (const j of i)
+            j.style.minHeight = `0px`;
+
+        const maxHeight = Math.ceil(i.reduce((a, i) => Math.max(a, i.clientHeight), 0));
 
         for (const j of i)
             j.style.minHeight = `${maxHeight}px`;
