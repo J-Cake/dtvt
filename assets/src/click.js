81fe2f68f4cf1ec89d5b09e1db1d67b7460509d6
jcake <jakieschneider13@gmail.com>
2022-04-22 10:45:27 +1000

    Easter egg. Luv u Steph

--- assets/src/click.js
+++ assets/src/click.js
@@ -1,4 +1,7 @@
 const sound = new Audio('/assets/res/sound_effect.mp3');
-sound.addEventListener('canplaythrough', function (e) {
-    window.addEventListener('click', () => sound.play());
+const sofunny = new Audio('/assets/res/so-funny.mp3');
+
+sound.addEventListener('canplaythrough', function () {
+    window.addEventListener('click', _ => sound.play());
+    document.querySelector('.sh, #sh').addEventListener('click', e => [e.stopPropagation(), sofunny.play()]);
 });
