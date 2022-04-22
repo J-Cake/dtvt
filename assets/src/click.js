3269254735c07518d16dd6954da3e85003f882f7
jcake <jakieschneider13@gmail.com>
2022-04-22 20:42:18 +1000

    More sounds

--- assets/src/click.js
+++ assets/src/click.js
@@ -1,5 +1,7 @@
 const sound = new Audio('/assets/res/sound_effect.mp3');
 const sofunny = new Audio('/assets/res/so-funny.mp3');
+const auuuuuuugh = new Audio('/assets/res/auuuuuuugh.mp3');
+const waduhek = new Audio('/assets/res/waduhek.mp3');
 
 sound.volume = 0.5;
 sofunny.volume = 0.5;
@@ -9,4 +11,12 @@ sound.addEventListener('canplaythrough', function () {
     for (const i of document.querySelectorAll('.sh, #sh, img.home-img, #collapse-Stephanie'))
         if (i)
             i.addEventListener('click', e => [e.stopPropagation(), sofunny.play()]);
+
+    for (const i of document.querySelectorAll('.pb, #pb, #collapse-Phi'))
+        if (i)
+            i.addEventListener('click', e => [e.stopPropagation(), waduhek.play()]);
+
+    for (const i of document.querySelectorAll('.cp, #cp, #collapse-Clayton'))
+        if (i)
+            i.addEventListener('click', e => [e.stopPropagation(), auuuuuuugh.play()]);
 });
